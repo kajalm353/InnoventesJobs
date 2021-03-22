972ae4984c7de70ab10cefb215768eac944e6b30
kajalm353 <67140320+kajalm353@users.noreply.github.com>
2021-03-22 11:39:46 +0530

    Add files via upload

--- Musicians.js
+++ Musicians.js
@@ -1,64 +1,20 @@
-const mongoose = require("mongoose");
-const express = require("express");
-let Musicians = require("../models/Musicians");
-const router = express.Router();
+/**
+ * This is the musicians model.
+ * Creating schema for musicians
+ *
+ * @class musicians 
+ */
+ var mongoose = require('mongoose')
 
-
-router.post('/', async (req,res) => {
-    try {
-  
-          const musicians = new Musicians(req.body)
-          await musicians.save()
-          res.status(200).send({message:'Musicians Record Created'})
-        
-    } catch (error) {
-        res.status(500).send({
-            message: 'Internal Server Error'
-        })        
-    }
-
-})
-
-router.put('/', async (req, res) => {
-    try {
-              var updateMusicians = await Musicians.findOneAndUpdate({ _id: req.query.musiciansId }, { $set: req.body }, { new: true })
-        res.status(200).send(updateMusicians);
-    } catch (e) {
-        res.status(500).send({
-            message: 'Internal Server Error'
-        })
-    }
-})
-
-
-router.get('/', async(req,res) => {
-    try {
-     let result = await Musicians.aggregate([
-         {
-             $match: {Musician_Name: req.body.Musician_Name}
-         },{
-             $lookup:  {
-                from: "music_album",
-                localField: "Musicalbumid",
-                foreignField: "_id",
-                as: "Album_docs"
-              }
-            
-         },
-         {
-            $sort: {"Album_docs.Price": 1}
-          }
-     ])
-
-     res.status(200).send(result)
-    
-    
-    } catch (error) {
-        res.status(500).send({
-            message: 'Internal Server Error'
-        })
-    }
-    
-    
-    })
-module.exports = router;
+ var musicianSchema = new mongoose.Schema(
+   {
+     Musicalbumid:{type: mongoose.Schema.Types.ObjectId, ref: 'music_album'},
+     Musician_Name : { type: String , required: true},
+     Musician_Type: {type: String, required:true}
+   })
+ 
+ 
+ var collectionName = 'musicians';
+ var musicians = mongoose.model('musicians', musicianSchema, collectionName);
+ 
+ module.exports = musicians;
